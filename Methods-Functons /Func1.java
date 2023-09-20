6c1187a47182582b84ed0018bb2646a3ee784b3a
Ganesh Jadhav <108882321+ganeshjadhav2402@users.noreply.github.com>
2023-09-20 13:54:29 +0530

    Update Func1.java

--- Methods-Functons /Func1.java
+++ Methods-Functons /Func1.java
@@ -1,2 +1,32 @@
 # function to multiply 2 numbers.
 
+import java.util.*;
+
+
+public class Functions {
+
+
+   //Multiply 2 numbers
+
+   public static int multiply(int a, int b) {
+
+        return a*b;
+
+   }
+
+   public static void main(String args[]) {
+
+       Scanner sc = new Scanner(System.in);
+
+       int a = sc.nextInt();
+
+       int b = sc.nextInt();
+
+
+       int result = multiply(a, b);
+
+       System.out.println(result);
+
+   }
+
+}  
