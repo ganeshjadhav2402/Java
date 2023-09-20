4897afa37e28010454c78ee1143117144a0503b1
Ganesh Jadhav <108882321+ganeshjadhav2402@users.noreply.github.com>
2023-09-20 14:05:43 +0530

    Update func2.java

--- Methods-Functons /func2.java
+++ Methods-Functons /func2.java
@@ -1,1 +1,26 @@
+# function to calculate the product of 2 numbers.
 
+import java.util.*;
+
+
+public class Functions {
+
+    public static int calculateProduct(int a, int b) {
+
+       return a * b;
+
+    }
+
+        public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        int b = sc.nextInt();
+
+        System.out.println(calculateProduct(a, b));
+
+    }
+
+}
