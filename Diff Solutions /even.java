aad0ee74fcd81375e416f2868e9f2e5954ee6ba0
Ganesh Jadhav <108882321+ganeshjadhav2402@users.noreply.github.com>
2023-09-20 12:08:00 +0530

    Update and rename Diff Solutions to Diff Solutions /even.java

---
+++ Diff Solutions /even.java
@@ -0,0 +1,22 @@
+
+public class Solutions {
+
+2. public static void main(String args[]) {
+
+3. int n = 25;
+
+4.
+
+5. for(int i=1; i<=n; i++) {
+
+6. if(i % 2 == 0) {
+
+7. System.out.println(i);
+
+8. }
+
+9. }
+
+10. }
+
+11. }
