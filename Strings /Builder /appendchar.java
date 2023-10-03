45a2d7bfcb6b071cac31788eb9a8d0838e6f19ef
Ganesh Jadhav <108882321+ganeshjadhav2402@users.noreply.github.com>
2023-10-03 15:14:05 +0530

    Create appendchar.java

---
+++ Strings /Builder /appendchar.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+
+public class Strings {
+  
+    public static void main(String args[]) {
+      
+      StringBuilder sb = new StringBuilder("Tony");
+
+      sb.append(" Stark");
+  
+      System.out.println(sb);
+    }
+}
