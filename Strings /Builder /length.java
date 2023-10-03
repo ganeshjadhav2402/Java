89121dadc0cdd7f551f746e1d05eb094bf52a5c7
Ganesh Jadhav <108882321+ganeshjadhav2402@users.noreply.github.com>
2023-10-03 15:30:24 +0530

    Update length.java write

--- Strings /Builder /length.java
+++ Strings /Builder /length.java
@@ -2,10 +2,18 @@ import java.util.*;
 
 
 public class Strings {
-public static void main(String args[]) {
-StringBuilder sb = new StringBuilder("Tony");
-sb.append(" Stark");
-System.out.println(sb);
-System.out.println(sb.length());
-}
+
+    public static void main(String args[]) {
+
+      StringBuilder sb = new StringBuilder("Tony");
+
+      sb.append(" Stark");
+
+      System.out.println(sb);
+
+      
+      System.out.println(sb.length());
+
+    }
+
 }
