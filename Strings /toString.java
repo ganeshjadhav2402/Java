bcceb75b57821dcf265899d07448570e4a179196
Ganesh Jadhav <108882321+ganeshjadhav2402@users.noreply.github.com>
2023-10-03 15:40:10 +0530

    Update toString.java

--- Strings /toString.java
+++ Strings /toString.java
@@ -1,9 +1,16 @@
 import java.util.*;
 
+
 public class Strings {
-public static void main(String args[]) {
-int number = 123;
-String str = Integer.toString(number);
-System.out.println(str.length());
-}
+
+  public static void main(String args[]) {
+
+    int number = 123;
+
+    String str = Integer.toString(number);
+
+    System.out.println(str.length());
+
+  }
+  
 }
