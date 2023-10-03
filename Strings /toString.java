a14d62c036dbf1a0284e9c57b5f676d65b7a3526
Ganesh Jadhav <108882321+ganeshjadhav2402@users.noreply.github.com>
2023-10-03 15:01:40 +0530

    Create toString.java

---
+++ Strings /toString.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Strings {
+public static void main(String args[]) {
+int number = 123;
+String str = Integer.toString(number);
+System.out.println(str.length());
+}
+}
