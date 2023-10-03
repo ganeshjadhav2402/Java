3b7ec0cb5170074bbc783e8719d7fa828c3f89de
Ganesh Jadhav <108882321+ganeshjadhav2402@users.noreply.github.com>
2023-10-03 15:37:52 +0530

    Update compare.java

--- Strings /compare.java
+++ Strings /compare.java
@@ -2,26 +2,51 @@ import java.util.*;
 
 
 public class Strings {
-public static void main(String args[]) {
-String name1 = "Ganesh";
-String name2 = "Ganesh";
-if(name1.equals(name2)) {
-System.out.println("They are the same string");
-} else {
-System.out.println("They are different strings");
-}
-//DO NOT USE == to check for string equality
-//Gives correct answer here
-if(name1 == name2) {
-System.out.println("They are the same string");
-} else {
-System.out.println("They are different strings");
-}
-//Gives incorrect answer here
-if(new String("Ganesh") == new String("Ganesh")) {
-System.out.println("They are the same string");
-} else {
-System.out.println("They are different strings");
-}
-}
+
+  public static void main(String args[]) {
+
+    String name1 = "Ganesh";
+
+    String name2 = "Ganesh";
+
+    if(name1.equals(name2)) {
+
+      System.out.println("They are the same string");
+
+    } else {
+
+      System.out.println("They are different strings");
+
+    }
+
+    
+    //DO NOT USE == to check for string equality
+
+    //Gives correct answer here
+
+    if(name1 == name2) {
+
+      System.out.println("They are the same string");
+
+    } else {
+
+      System.out.println("They are different strings");
+
+    }
+
+    
+    //Gives incorrect answer here
+
+    if(new String("Ganesh") == new String("Ganesh")) {
+
+      System.out.println("They are the same string");
+
+    } else {
+
+      System.out.println("They are different strings");
+
+    }
+
+  }
+
 }
