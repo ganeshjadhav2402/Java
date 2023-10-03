f4827a02449965492564e0688eb8242ccd0f5b89
Ganesh Jadhav <108882321+ganeshjadhav2402@users.noreply.github.com>
2023-10-03 15:38:21 +0530

    Update substring.java

--- Strings /substring.java
+++ Strings /substring.java
@@ -1,8 +1,13 @@
 import java.util.*;
 
 public class Strings {
-public static void main(String args[]) {
-String name = "GaneshJadhav";
-System.out.println(name.substring(0, 4));
+
+  public static void main(String args[]) {
+
+    String name = "GaneshJadhav";
+
+    System.out.println(name.substring(0, 4));
+ 
   }
+
 }
