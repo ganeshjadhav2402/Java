0adfeb292476f3dc7aba23bfd2eeee06cc7870a6
Ganesh Jadhav <108882321+ganeshjadhav2402@users.noreply.github.com>
2023-10-03 15:12:10 +0530

    Create insertchar.java

---
+++ Strings /Builder /insertchar.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+
+public class Strings {
+public static void main(String args[]) {
+StringBuilder sb = new StringBuilder("tony");
+//Insert char
+sb.insert(0, 'S');
+System.out.println(sb);
+}
+}
