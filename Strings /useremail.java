2efa055fbcd50e7598d0485ceb79364a5e6a3956
Ganesh Jadhav <108882321+ganeshjadhav2402@users.noreply.github.com>
2023-10-03 15:40:53 +0530

    Update useremail.java

--- Strings /useremail.java
+++ Strings /useremail.java
@@ -2,17 +2,31 @@ import java.util.*;
 
 
 public class Strings {
-public static void main(String args[]) {
-Scanner sc = new Scanner (System.in);
-String email = sc.next();
-String userName = "";
-for(int i=0; i<email.length(); i++) {
-if(email.charAt(i) == '@') {
-break;
-} else {
-userName += email.charAt(i);
-}
-}
-System.out.println(userName);
-}
+
+  public static void main(String args[]) {
+
+    Scanner sc = new Scanner (System.in);
+
+    String email = sc.next();
+
+    String userName = "";
+
+    for(int i=0; i<email.length(); i++) {
+
+      if(email.charAt(i) == '@') {
+
+        break;
+
+      } else {
+
+        userName += email.charAt(i);
+
+      }
+
+    }
+
+    System.out.println(userName);
+
+  }
+
 }
