1a24565ecabd52232313ce055e1c6c5c303b14bb
Ganesh Jadhav <108882321+ganeshjadhav2402@users.noreply.github.com>
2023-10-03 15:31:55 +0530

    Update deletechar.java write

--- Strings /Builder /deletechar.java
+++ Strings /Builder /deletechar.java
@@ -2,13 +2,24 @@ import java.util.*;
 
 
 public class Strings {
-public static void main(String args[]) {
-StringBuilder sb = new StringBuilder("tony");
-//Insert char
-sb.insert(0, 'S');
-System.out.println(sb);
-//delete char
-sb.delete(0, 1);
-System.out.println(sb);
-}
+
+  public static void main(String args[]) {
+
+    StringBuilder sb = new StringBuilder("tony");
+
+    //Insert char
+
+    sb.insert(0, 'S');
+
+    System.out.println(sb);
+
+    
+    //delete char
+
+    sb.delete(0, 1);
+
+    System.out.println(sb);
+
+  }
+
 }
